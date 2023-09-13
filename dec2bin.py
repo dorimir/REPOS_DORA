aa554e66d27f2b40fc1e552f893510835a537329
dorimir <144936659+dorimir@users.noreply.github.com>
2023-09-13 18:47:36 +0200

    Update dec2bin.py

--- dec2bin.py
+++ dec2bin.py
@@ -7,7 +7,7 @@
 # ------------------------------------------------------
 def dec2bin(numero_decimal, numero_bits):
     numero_binario = bin(numero_decimal)
-    if numero_decimal >= 0:
+    if numero_decimal >= 0
     numero_binario = numero_binario[2:len(numero_binario)]  # quita el "0b" del principio
     
     while len(numero_binario) < numero_bits:      # añade 0's a la izquierda si hace falta
